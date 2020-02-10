1098a9787a5af28e208e9e7a5d33746c95fed219
Arun Ramachandriya <Arun.Ramachandriya@investec.co.uk>
2020-02-10 09:07:58 +0000

    Mutations: add/remove star

--- client/project/index.js
+++ client/project/index.js
@@ -23,8 +23,10 @@ let queryRepoList = `
         orderBy: {field: CREATED_AT, direction: DESC}) {
       totalCount
       nodes {
-        name
+        id
+        name        
         description
+        viewerHasStarred
         issues {
           totalCount
         }
@@ -35,11 +37,35 @@ let queryRepoList = `
 }
 ` + commitFragment;
 
-let mutationAddStar;
+const mutationAddStar = `
+mutation($id: ID!) {
+  addStar(input: {starrableId: $id}) {
+    clientMutationId
+    starrable {
+      id
+      ... on Repository {
+        name
+        viewerHasStarred
+      }
+    }
+  }
+}`;
 
-let mutationRemoveStar;
+let mutationRemoveStar = `
+mutation($id: ID!) {
+  removeStar(input: {starrableId: $id}) {
+    clientMutationId
+    starrable {
+      id
+      ... on Repository {
+        name
+        viewerHasStarred
+      }
+    }
+  }
+}`;
 
-function gplRequest(query, variables, onSuccess) {
+function gqlRequest(query, variables, onSuccess) {
   var github_token = "33c567a9027eab39a55fd0278e429f38dd9bc929";
 
   $.post({
@@ -53,8 +79,12 @@ function gplRequest(query, variables, onSuccess) {
       variables: variables
     }),
     success: (response) => {
-      console.log(response);
-      onSuccess(response)
+      if (response.errors) {
+        console.error(response.errors);
+      } else {
+        console.log(response);
+        onSuccess(response.data);
+      }
     },
     error: (error) => console.log(error)
   });
@@ -63,25 +93,53 @@ function gplRequest(query, variables, onSuccess) {
 
 function starHandler(element) {
   // STAR OR UNSTAR REPO BASED ON ELEMENT STATE
-
+//  console.log(element);
+ if ($(element).text() == emptyStar) {
+   console.log('To be starred')
+   gqlRequest(
+     mutationAddStar,
+     { id: element.id },
+     data => {
+      if (data.addStar.starrable.viewerHasStarred) {
+        $(element).text(fullStar);
+      }
+     }
+    );
+ } 
+ else {
+  console.log('To be un-starred');
+  gqlRequest(
+    mutationRemoveStar,
+    { id: element.id },
+    data => {
+      if (!data.removeStar.starrable.viewerHasStarred) {
+        $(element).text(emptyStar);
+      }
+    }
+   );  
+ }
 }
 
 $(window).ready(function () {
   // GET NAME AND REPOSITORIES FOR VIEWER
-  gplRequest(queryRepoList,
+  gqlRequest(queryRepoList,
     {},
-    (response) => {
-      const name = response.data.viewer.name;
+    (data) => {
+      const name = data.viewer.name;
       $('header h2').text(`Hello ${name}`);
 
-      const repos = response.data.viewer.repos;
+      const repos = data.viewer.repos;
       if (repos.totalCount > 0) {
         $("ul.repos").empty();
 
         repos.nodes.forEach(node => {
+          const star = node.viewerHasStarred? fullStar: emptyStar;
           const repoElement = `
           <div>
-            <h3>${node.name}</h3>
+            <h3>
+              ${node.name}
+              <span id=${node.id} class="star" onClick="starHandler(this)">${star}</span>
+            </h3>
             <p>${node.description? node.description : 'n/a'}</p>
             <p>${node.commits.target.history.totalCount} commits</p>
           </div>
