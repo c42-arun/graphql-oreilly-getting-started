2be26e293102283c4e3bc2d3edb65f70136e944e
Arun Ramachandriya <Arun.Ramachandriya@investec.co.uk>
2020-02-10 08:01:20 +0000

    Show list of repositories in card view

--- client/project/index.js
+++ client/project/index.js
@@ -15,14 +15,25 @@ fragment commitFragment on Repository {
 }
 `;
 
-let queryRepoList;
+let queryRepoList = `
+{ 
+  viewer { 
+    name
+    repos: repositories (first: 10) {
+      totalCount
+      nodes {
+        name
+      }
+    }
+  }
+}`;
 
 let mutationAddStar;
 
 let mutationRemoveStar;
 
 function gplRequest(query, variables, onSuccess) {
-  var github_token = "044996d2845d6d496f8e3bd5dd32cd80e1b6bb04";
+  var github_token = "33c567a9027eab39a55fd0278e429f38dd9bc929";
 
   $.post({
     url: "https://api.github.com/graphql",
@@ -48,18 +59,28 @@ function starHandler(element) {
 
 }
 
-$(window).ready(function() {
+$(window).ready(function () {
   // GET NAME AND REPOSITORIES FOR VIEWER
-  gplRequest(
-    `{
-      viewer {
-      name
-      }
-    }`, 
+  gplRequest(queryRepoList,
     {},
     (response) => {
-      console.log('at window.ready(): ' + response.data.viewer.name);
-      $('header h2').text(`Hello ${response.data.viewer.name}`)
+      const name = response.data.viewer.name;
+      $('header h2').text(`Hello ${name}`);
+
+      const repos = response.data.viewer.repos;
+      if (repos.totalCount > 0) {
+        $("ul.repos").empty();
+
+        repos.nodes.forEach(node => {
+          const repoElement = `
+          <div>
+            <h3>${node.name}</h3>
+          </div>
+        `;
+
+          $("ul.repos").append(`<li>${repoElement}</li>`);
+        });
+      }
     }
   );
 });
