9f5705ed2a89e784ce602e0ca63ae301d5e8e795
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2020-02-12 07:34:58 +0000

    Returning a dummy user object

--- node_server/project/src/schema.js
+++ node_server/project/src/schema.js
@@ -1,12 +1,25 @@
 const graphql = require('graphql');
 
+const userType = new graphql.GraphQLObjectType({
+    name: 'User',
+    description: 'A user in the system',
+    fields: {
+        id: {
+            type: graphql.GraphQLID,
+            resolve(user) {
+                return user.id;
+            }
+        }
+    }
+});
+
 const queryType = new graphql.GraphQLObjectType({
     name: 'Query',
     fields: {
-        hello: {
-            type: graphql.GraphQLString,
+        users: {
+            type: graphql.GraphQLList(userType),
             resolve(root) {
-                return 'world';
+                return [{id: 'abc'}];
             }
         }
     }
